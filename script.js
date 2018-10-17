c0399768f86b873db3627872065a5610851d039c
Clark Brown <clarkbrown@byu.net>
2018-10-17 22:38:08 +0000

    Basic working version 1 with some styling

--- script.js
+++ script.js
@@ -1,74 +1,122 @@
-/**
- * 1. We have added a directive with the name 'avatar' and handler of
- * avatarDirective to our angular app module
- */
+var key = "MmFmNmNiYzEtZTkzOS00ODZjLTg2ZmMtZTVlNTk3MGE3ZDAw";
+var trackLimit = 6;
+
 angular.module('app', [])
     .controller('mainCtrl', mainCtrl)
-    .directive('user', userDirective);
+    .directive('song', songDirective)
+    .config(function($sceDelegateProvider) {$sceDelegateProvider.resourceUrlWhitelist(['**']);});
 
-function mainCtrl($scope) {
+function mainCtrl($scope, $http) {
 
-    $scope.users = [];
-
-    $scope.addNew = function(user) {
+    $scope.currentYear = 0;
+    $scope.currentTrack = '';
+    $scope.url = '';
+    $scope.tracks = [];
+    $scope.isPlaying = false;
+    
+    $scope.getMusic = function(year) {
+        $(".song-list").fadeOut(250);
+        console.log(year.year);
         
-        if(!user.name && !user.email) {
-            $("div.error").fadeIn(300).delay(3000).fadeOut(400);
+        if($scope.currentYear > 0) {
+            $("#year-"+$scope.currentYear).toggleClass("active-year");
+        }
+        
+        $scope.currentYear = year.year;
+        $("#year-"+$scope.currentYear).toggleClass("active-year");
+        
+        
+        for ($scope.i = 0; $scope.i < $scope.timeline.length; $scope.i++) {
+            if ($scope.timeline[$scope.i].year == $scope.currentYear) {
+                $scope.yearObj = $scope.timeline[$scope.i];
+            }
+        }
+        
+        $scope.url = "https://api.napster.com/v2.2/playlists/pp." + $scope.yearObj.pid
+            + "/tracks?apikey=" + key + "&limit=" + trackLimit;
+            
+        console.log($scope.url);
+        
+        $http.get($scope.url).then(function(response) {
+                console.log(response);
+                $scope.tracks = response.data.tracks;
+                console.log($scope.tracks);
+                $(".song-list").fadeIn(250);
+        });
+        
+    };
+    
+    $scope.togglePlayback = function(id) {
+        var audio = "#"+id;
+        var currentAudio = "#"+$scope.currentTrack;
+        
+        if ($scope.isPlaying) {
+            if($scope.currentTrack == id) {
+                $(audio).trigger("pause");
+                $scope.isPlaying = false; 
+            }
+            else {
+                $(currentAudio).trigger("pause");
+                $(currentAudio).parent().parent().toggleClass("active-song");
+                $(currentAudio).parent().fadeOut(250);
+                $(audio).trigger("play");
+                $(audio).parent().parent().toggleClass("active-song");
+                $scope.isPlaying = true;
+                $scope.currentTrack = id;
+            }
         }
         else {
-            $scope.users.push({
-                name: user.name,
-                avatarUrl: user.url,
-                email: user.email
-            }); /* [1] */
-    
-            user.name = ''; /* [2] */
-            user.url = '';
-            user.email = '';            
+            $(audio).trigger("play");
+            $scope.isPlaying = true;
+            $scope.currentTrack = id;
+            $(audio).parent().parent().toggleClass("active-song");
         }
-
     };
+
+    $scope.showInfo = function(id) {
+        $("#"+id).parent().fadeIn(250);
+    };
+    
+    $scope.hideInfo = function(id) {
+        if(id != $scope.currentTrack) {
+            $("#"+id).parent().fadeOut(250);
+        }
+    };
+    
+    $scope.timeline = [
+        {year: 1920, pid: 139422201},
+        {year: 1930, pid: 139422201},
+        {year: 1940, pid: 139422201},
+        {year: 1950, pid: 139422201},
+        {year: 1960, pid: 139422201},
+        {year: 1970, pid: 139422201},
+        {year: 1980, pid: 139422201},
+        {year: 1990, pid: 139422201},
+        {year: 2000, pid: 139422201},
+        {year: 2005, pid: 139422201},
+        {year: 2010, pid: 139422201},
+        {year: 2015, pid: 139422201}
+    ];
 }
 
-/**
- * 1. this defines the api of our avatar directive. This means we are
- * expecting a user property whose value should be interpreted as an object.
- * 2. This simply means we want this directive to be used as an element.
- * 3. You can see here we've moved the html that was in our template before
- * and give it as the template for this directive. This means wherever we use
- * <avatar /> this html will also be placed there.
- * 4. Here we are implementing the feature where if there is no user avatar url,
- * we go ahead and give it a default
- */
-function userDirective() {
+function songDirective() {
     return {
         scope: {
-            user: '=' /* [1] */
+            sid: '='
         },
         restrict: 'E',
-        /* [2] */
         replace: 'true',
         template: (
-            '<div class="displayUser">' +
-            '<img ng-src="{{user.avatarUrl}}" />' +
-            '<h4>{{user.name}}</h4>' +
-            '<h6>Email: <a href="mailto:{{user.email}}?Subject=Hello" target="_top">{{user.email}}</a></h6>' +
+            '<div class="song">' +
+                '<img src="http://direct.rhapsody.com/imageserver/v2/albums/{{track.albumId}}/images/300x300.jpg' +
+                '<h2>{{track.name}}</h2>' +
             '</div>'
         ),
-        /* [3] */
         link: link
     };
 
-    function link(scope) { /* [4] */
-        if (!scope.user.avatarUrl) {
-            scope.user.avatarUrl = 'https://www.drupal.org/files/issues/default-avatar.png';
-        }
-        if (!scope.user.email) {
-            scope.user.email = "none";
-        }
-        if (!scope.user.name) {
-            scope.user.name = "NO NAME";
-        }
+    function link(scope) {
+        
     }
 
 }
